5008b04351ee78bf9b77ec0f6696dd678303989e
filippo <schianchi.filippo.93@gmail.com>
2019-11-11 18:53:46 +0100

    layout completato

--- javascript/script.js
+++ javascript/script.js
@@ -6,19 +6,25 @@ $( document ).ready(function() {
         //ci salviamo il valore dell'input inserito dall'utente
         var messaggio = $(".messaggio").val();
         console.log(messaggio);
+        if(messaggio.length != 0) {
+
+            // cloniamo (facciamo una copia) del div con classe "msgsent" che sta dentro un div con ID "template"
+            var elmentmsg = $("#template .msgsent").clone();
+
+
+            // modifica questa copia di "msgsent" aggiungendogli il testo del messaggio
+            elmentmsg.find(".testo").text(messaggio);
+            
+            // appendiamo una copia con testo valorizzato del div "msgsent"
+            $("#container").append(elmentmsg);
+
+            // ripuliamo il contenuto dell'input, per UX
+            $(".messaggio").val("");
+        } else {
+            return;
+        }
         
-        // cloniamo (facciamo una copia) del div con classe "msgsent" che sta dentro un div con ID "template"
-        var elmentmsg = $("#template .msgsent").clone();
-
-
-        // modifica questa copia di "msgsent" aggiungendogli il testo del messaggio
-        elmentmsg.find(".testo").text(messaggio);
         
-        // appendiamo una copia con testo valorizzato del div "msgsent"
-        $("#container").append(elmentmsg);
-
-        // ripuliamo il contenuto dell'input, per UX
-        $(".messaggio").val("");
     })
 
 /* cambio di icon quando clicco sul input */
@@ -31,8 +37,16 @@ $( document ).ready(function() {
     $('body').click(function(evt){    
         if(evt.target.id == "messaggio")
            return; 
-        $(".vocale").css("display", "inline-block");
-        $(".send").css("display", "none")
+        var messaggio = $(".messaggio").val();
+        if(messaggio.length == 0) {
+            $(".vocale").css("display", "inline-block");
+            $(".send").css("display", "none");
+        } else {
+            $(".vocale").css("display", "none");
+            $(".send").css("display", "inline-block")
+
+        }
+    
  
  });
 

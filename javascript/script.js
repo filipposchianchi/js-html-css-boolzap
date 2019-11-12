7d739e1c1e6de443b039f59abe8d9f9a05c032fa
filippo <schianchi.filippo.93@gmail.com>
2019-11-12 12:52:53 +0100

    aggiunto invio messaggio tramite tasto "invio"

--- javascript/script.js
+++ javascript/script.js
@@ -3,36 +3,27 @@ $( document ).ready(function() {
     //agganciamo al click sul bottone la funzione di callbnack
     $(".invio").click(function(){
 
-        //ci salviamo il valore dell'input inserito dall'utente
-        var messaggio = $(".messaggio").val();
-        console.log(messaggio);
-        if(messaggio.length != 0) {
-
-            // cloniamo (facciamo una copia) del div con classe "msgsent" che sta dentro un div con ID "template"
-            var elmentmsg = $("#template .msgsent").clone();
-
-
-            // modifica questa copia di "msgsent" aggiungendogli il testo del messaggio
-            elmentmsg.find(".testo").text(messaggio);
-            
-            // appendiamo una copia con testo valorizzato del div "msgsent"
-            $("#conversazione").append(elmentmsg);
-
-            // ripuliamo il contenuto dell'input, per UX
-            $(".messaggio").val("");
-        } else {
-            return;
-        } 
-
+        invioMessaggio();
     })
 
-/* cambio di icon quando clicco sul input */
+    $(window).on({
+        keyup: function (k) {            
+            if (k.keyCode == "13") {
+                invioMessaggio();
+            }
+            
+        }
+    })
+
+
+    
+    /* cambio di icon quando clicco sul input */
     $(".messaggio").click(function (){
         $(".vocale").css("display", "none");
         $(".send").css("display", "inline-block")
     })
 
-/* ritorno all'icon del vocale se clicco fuori dall input*/
+    /* ritorno all'icon del vocale se clicco fuori dall input*/
     $('body').click(function(evt){    
         if(evt.target.id == "messaggio")
            return; 
@@ -45,11 +36,34 @@ $( document ).ready(function() {
             $(".send").css("display", "inline-block")
 
         }
-    
- 
  });
 
-    
+   
+ 
+ function invioMessaggio() {
+
+    //ci salviamo il valore dell'input inserito dall'utente
+    var messaggio = $(".messaggio").val();
+    console.log(messaggio);
+    if(messaggio.length != 0) {
+
+        // cloniamo (facciamo una copia) del div con classe "msgsent" che sta dentro un div con ID "template"
+        var elmentmsg = $("#template .msgsent").clone();
+
+
+        // modifica questa copia di "msgsent" aggiungendogli il testo del messaggio
+        elmentmsg.find(".testo").text(messaggio);
+        
+        // appendiamo una copia con testo valorizzato del div "msgsent"
+        $("#conversazione").append(elmentmsg);
+
+        // ripuliamo il contenuto dell'input, per UX
+        $(".messaggio").val("");
+    } else {
+        return;
+    } 
+}
+
 
 });
 
